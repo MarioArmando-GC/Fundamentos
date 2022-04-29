5dcbc8f0c2a5432083b7a422bb927313623a4b23
Mario Armando González Castellanos <marioarmandogc.008@gmail.com>
2022-04-28 21:33:31 -0500

    Delete Practica5MarioArmandoGonzalezCastellanos.js

--- Practica5MarioArmandoGonzalezCastellanos.js
+++
@@ -1,8 +0,0 @@
-let matriz = [["Matriz original"], [1, 2, 3], [4, 5, 6], [7, 8, 9]];
-let salida1 = [["salida 1"], [9, 8, 7], [6, 5, 4], [3, 2, 1]];
-let salida2 = [["salida 2"], [1, 4, 7], [2, 5, 8], [3, 6, 9]];
-let salida3 = [["salida 3"], [9, 6, 3], [8, 5, 2], [7, 4, 1]];
-let todos = [matriz, salida1, salida2, salida3];
-for (let i = 1; i <= 1; i++) {
-  console.log(todos);
-}
