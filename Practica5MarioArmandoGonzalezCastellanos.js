3039d8b9e1a623554b3bba92faa7dcc2fbd95560
Mario Armando González Castellanos <marioarmandogc.008@gmail.com>
2022-05-24 17:57:11 -0500

    Add files via upload

--- Practica5MarioArmandoGonzalezCastellanos.js
+++ Practica5MarioArmandoGonzalezCastellanos.js
@@ -1,8 +1,27 @@
-let matriz = [["Matriz original"], [1, 2, 3], [4, 5, 6], [7, 8, 9]];
-let salida1 = [["salida 1"], [9, 8, 7], [6, 5, 4], [3, 2, 1]];
-let salida2 = [["salida 2"], [1, 4, 7], [2, 5, 8], [3, 6, 9]];
-let salida3 = [["salida 3"], [9, 6, 3], [8, 5, 2], [7, 4, 1]];
-let todos = [matriz, salida1, salida2, salida3];
-for (let i = 1; i <= 1; i++) {
-  console.log(todos);
+const matris = new Array([1, 2, 3], [4, 5, 6], [7, 8, 9]);
+console.log(matris.length, "Matriz original");
+
+for (let i = 0; i < matris.length; i++) {
+  console.log(matris[i]);
+}
+//***************************************************** */
+const matris2 = new Array([9, 8, 7], [6, 5, 4], [3, 2, 1]);
+console.log(matris2.length, "salida 1");
+
+for (let i = 0; i < matris2.length; i++) {
+  console.log(matris2[i]);
+}
+//**************************************************** */
+const matris3 = new Array([1, 4, 7], [2, 5, 8], [3, 6, 9]);
+console.log(matris3.length, "salida 2");
+
+for (let i = 0; i < matris3.length; i++) {
+  console.log(matris3[i]);
+}
+//***************************************************** */
+const matris4 = new Array([9, 6, 3], [8, 5, 2], [7, 4, 1]);
+console.log(matris4.length, "salida 3");
+
+for (let i = 0; i < matris4.length; i++) {
+  console.log(matris4[i]);
 }
